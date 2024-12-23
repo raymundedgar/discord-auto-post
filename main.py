95543b074fc4821a28a3b62c4694ed4da3fe273b
Raymund Edgar <dev.raymund@gmail.com>
2024-12-24 00:56:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,91 +30,64 @@ all_thread_names = []
 
 def new_thread():
     global all_thread_names
-    token = input("[X] Вставьте токен юзера от которого будет производится отправка\n >> ")
+    token = input("[X] Insert the token of the user from which the sending will be performed\n >> ")
     channelid = input(
-        "[X] Вставьте id канала в который будут отправляться сообщения (пользователь должен находиться на сервере)\n >> ")
-    message = input("[X] Введите сообщение которое нужно отправлять в чат\n >> ")
-    timer = input("[X] Введите переодичность отправки в секундах\n >> ")
-    name = input("[X] Введите уникальное название процесса\n >> ")
+        "[X] Insert the channel ID where messages will be sent (the user must be on the server)\n >> ")
+    message = input("[X] Enter the message to be sent in the chat\n >> ")
+    timer = input("[X] Enter the sending interval in seconds\n >> ")
+    name = input("[X] Enter a unique name for the process\n >> ")
     if name in all_thread_names:
-        print(Fore.RED + '\nERROR: Такой процесс  уже существует, введите другое название')
+        print(Fore.RED + '\nERROR: A process with this name already exists, please enter a different name')
         print(Style.RESET_ALL)
     else:
         new_con = Connection(token, channelid, message, timer, name)
         new_con.send_message()
         all_threads[new_con.name] = new_con
         all_thread_names.append(new_con.name)
-        print(Fore.GREEN + 'Процесс запущен!\n')
+        print(Fore.GREEN + 'Process started!\n')
         print(Style.RESET_ALL)
 
 
 def table_thread():
     global all_thread_names
     file_token = open('token.txt', 'r', encoding='utf-8')
-    list_token = []
-    for line in file_token:
-        list_token.append(line.strip())
+    list_token = [line.strip() for line in file_token]
     file_channel = open('channel.txt', 'r', encoding='utf-8')
-    list_channel = []
-    for line in file_channel:
-        list_channel.append(line.strip())
-    message = input('Введите сообщение которое будет отправлено\n >> ')
-    timer = input("[X] Введите переодичность отправки в секундах\n >> ")
-    for line_token in list_token:
-        for line_channel in list_channel:
-            token = line_token
-            channelid = line_channel
+    list_channel = [line.strip() for line in file_channel]
+    message = input('Enter the message to be sent\n >> ')
+    timer = input("[X] Enter the sending interval in seconds\n >> ")
+    for token in list_token:
+        for channelid in list_channel:
             name = f'{token}_{channelid}'
             if name in all_thread_names:
-                print(Fore.RED + '\nERROR: Такой процесс  уже существует')
+                print(Fore.RED + '\nERROR: A process with this name already exists')
                 print(Style.RESET_ALL)
             else:
                 new_con = Connection(token, channelid, message, timer, name)
                 new_con.send_message()
                 all_threads[new_con.name] = new_con
                 all_thread_names.append(new_con.name)
-                print(Fore.GREEN + 'Процесс запущен!\n')
+                print(Fore.GREEN + 'Process started!\n')
                 print(Style.RESET_ALL)
 
 
 def random_message(choise):
     global all_thread_names
-    if choise == 1:
-        token = input("[X] Вставьте токен юзера от которого будет производится отправка\n >> ")
-        channelid = input(
-            "[X] Вставьте id канала в который будут отправляться сообщения (пользователь должен находиться на сервере)\n >> ")
-        timer = input("[X] Введите переодичность отправки в секундах\n >> ")
-        name = input("[X] Введите уникальное название процесса\n >> ")
-        if name in all_thread_names:
-            print(Fore.RED + '\nERROR: Такой процесс  уже существует, введите другое название')
-            print(Style.RESET_ALL)
-        else:
-            message = None
-            new_con = Connection(token, channelid, message, timer, name, random=1)
-            new_con.send_message()
-            all_threads[new_con.name] = new_con
-            all_thread_names.append(new_con.name)
-            print(Fore.GREEN + 'Процесс запущен!\n')
-            print(Style.RESET_ALL)
-    elif choise == 2:
-        token = input("[X] Вставьте токен юзера от которого будет производится отправка\n >> ")
-        channelid = input(
-            "[X] Вставьте id канала в который будут отправляться сообщения (пользователь должен находиться на сервере)\n >> ")
-        timer = input("[X] Введите переодичность отправки в секундах\n >> ")
-        name = input("[X] Введите уникальное название процесса\n >> ")
-        if name in all_thread_names:
-            print(Fore.RED + '\nERROR: Такой процесс  уже существует, введите другое название')
-            print(Style.RESET_ALL)
-        else:
-            message = None
-            new_con = Connection(token, channelid, message, timer, name, random=2)
-            new_con.send_message()
-            all_threads[new_con.name] = new_con
-            all_thread_names.append(new_con.name)
-            print(Fore.GREEN + 'Процесс запущен!\n')
-            print(Style.RESET_ALL)
+    token = input("[X] Insert the token of the user from which the sending will be performed\n >> ")
+    channelid = input(
+        "[X] Insert the channel ID where messages will be sent (the user must be on the server)\n >> ")
+    timer = input("[X] Enter the sending interval in seconds\n >> ")
+    name = input("[X] Enter a unique name for the process\n >> ")
+    if name in all_thread_names:
+        print(Fore.RED + '\nERROR: A process with this name already exists, please enter a different name')
+        print(Style.RESET_ALL)
     else:
-        print(Fore.RED + 'ERROR: такого варианта')
+        message = None
+        new_con = Connection(token, channelid, message, timer, name, random=choise)
+        new_con.send_message()
+        all_threads[new_con.name] = new_con
+        all_thread_names.append(new_con.name)
+        print(Fore.GREEN + 'Process started!\n')
         print(Style.RESET_ALL)
 
 
@@ -122,78 +95,63 @@ def main():
     global all_thread_names
     while True:
         print(
-            '1) Создать новый процесс\n2) Удалить процесс\n3) Просмотреть лог процесса\n4) Список процессов\n5) База токенов х база каналов х время\n6) Отправка рандом сообщения из файла\nq) Выход\n')
+            '1) Create a new process\n2) Delete a process\n3) View process log\n4) List all processes\n5) Token x Channel ID batch process\n6) Send random messages\nq) Exit\n')
         switch = input(' >> ')
         if switch == '1':
             new_thread()
         elif switch == '2':
-            print("Введите название процесса:\n")
-            name_thread = input(' >> ')
+            name_thread = input("Enter the process name:\n >> ")
             try:
-                buffer = all_threads[name_thread]
-                buffer.stop_thread()
+                all_threads[name_thread].stop_thread()
                 del all_threads[name_thread]
-                print('Удален из справочника процессов')
                 all_thread_names.remove(name_thread)
-                print('Удален из списка имен')
-            except KeyError:
-                print(Fore.RED + '\nERROR: такого процесса не существует')
+                print(Fore.GREEN + 'Process successfully deleted\n')
                 print(Style.RESET_ALL)
-
+            except KeyError:
+                print(Fore.RED + '\nERROR: No such process exists')
+                print(Style.RESET_ALL)
         elif switch == '3':
-            print("Введите название процесса:\n")
-            name_thread = input(' >> ')
+            name_thread = input("Enter the process name:\n >> ")
             try:
-                print(Fore.YELLOW + "Логи процесса " + name_thread + "\n---------------------")
-                buffer = all_threads[name_thread]
-                logs = buffer.get_log()
-                for i in logs:
-                    print(i)
+                logs = all_threads[name_thread].get_log()
+                print(Fore.YELLOW + f"Logs for process {name_thread}:\n---------------------")
+                print('\n'.join(logs))
                 print('---------------------')
                 print(Style.RESET_ALL)
             except KeyError:
-                print(Fore.RED + '\nERROR: такого процесса не существует')
+                print(Fore.RED + '\nERROR: No such process exists')
                 print(Style.RESET_ALL)
         elif switch == '4':
-            print('Все процессы:\n---------------------')
+            print('All processes:\n---------------------')
             get_all_threads_key()
             print('---------------------')
         elif switch == '5':
-            print('Режим создания процессов для большого  количество аккаунтов, с большим количеством каналов')
-            print('В файле token.txt укажите токены всех аккаунтов. Каждый токен на новой строке')
-            print('В файле channel.txt укажите id всех каналов, в которые должна происходить рассылка')
-            print('каждый id на новой строке(пользователь должен находиться на сервере)')
-            x = input("Напишите 'start', когда заполните файлы, для начала работы программы\n >> ")
-            if x == 'start':
-                print('Запуск')
+            print('Batch process creation for multiple accounts and channels')
+            print('In the file token.txt, specify the tokens for all accounts, one per line')
+            print('In the file channel.txt, specify the IDs of all channels, one per line')
+            if input("Type 'start' to begin once files are ready\n >> ").lower() == 'start':
                 table_thread()
             else:
-                print(Fore.RED + '\nERROR: Команды не существует, проверьте не опечатались ли вы')
+                print(Fore.RED + '\nERROR: Invalid command, please check your input')
                 print(Style.RESET_ALL)
         elif switch == '6':
-            print('Режим отправки рандомных сообщений')
-            print('Список сообщений, которые будут выбираться на угад запишите в файл message.txt')
-            print('Кажое новое сообщение на новой строке')
-            print(Fore.YELLOW + 'WARNING: если слишком часто писать сообщения, то вас могут забанить как бота')
+            print('Random message sending mode')
+            print('Write the list of messages to message.txt, one per line')
+            print(Fore.YELLOW + 'WARNING: Sending messages too frequently might result in a ban')
             print(Style.RESET_ALL)
-            print('')
-            print('1) Использовать обычный рандом')
-            print(
-                '2) Использовать умный рандом(Ни один аккаунт не повторит фразу с другим, пока не кончатся все сообщения в файле)')
-            x = input("\n >> ")
-            if x == '1':
-                print('Запуск')
+            print('1) Use basic randomization\n2) Use smart randomization (no repeated messages until all are sent)')
+            choice = input("\n >> ")
+            if choice == '1':
                 random_message(choise=1)
-            elif x == '2':
-                print('Запуск')
+            elif choice == '2':
                 random_message(choise=2)
             else:
-                print(Fore.RED + '\nERROR: Команды не существует, проверьте не опечатались ли вы')
+                print(Fore.RED + '\nERROR: Invalid choice, please check your input')
                 print(Style.RESET_ALL)
         elif switch == 'q':
             os.abort()
         else:
-            print(Fore.RED + '\nERROR: Указан неверный номер действия\n')
+            print(Fore.RED + '\nERROR: Invalid action number\n')
             print(Style.RESET_ALL)
 
 
